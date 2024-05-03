8199a72c73da725101bc2792e23d488cf771cb04
Ameet Toor <Ameet_Toor@Hotmail.com>
2024-05-03 09:19:25 -0700

    Re-add theme switching logic

--- init.lua
+++ init.lua
@@ -799,7 +799,12 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      local systemTheme = vim.fn.system 'defaults read -g AppleInterfaceStyle'
+      if string.find(systemTheme, 'Dark') then
+        vim.cmd.colorscheme 'tokyonight-night'
+      else
+        vim.cmd.colorscheme 'tokyonight-day'
+      end
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
