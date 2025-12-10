ef0fb1301eead20b4fd6262d1f62e7bc2af958a3
Ameet Toor <ameet2r@users.noreply.github.com>
2025-12-09 22:22:17 -0800

    Remove test change

--- init.lua
+++ init.lua
@@ -225,7 +225,7 @@ vim.opt.rtp:prepend(lazypath)
 --  To update plugins you can run
 --    :Lazy update
 --
--- NOTE: Here is where you install your plugins. test
+-- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
